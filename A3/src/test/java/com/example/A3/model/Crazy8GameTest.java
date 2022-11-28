f0f4d90f21aae4feae978ebb1ae9620bc3df57ab
danialross <92746365+danialross@users.noreply.github.com>
2022-11-28 17:12:10 -0500

    <refactor>

--- A3/src/test/java/com/example/A3/model/Crazy8GameTest.java
+++ A3/src/test/java/com/example/A3/model/Crazy8GameTest.java
@@ -17,30 +17,27 @@ class Crazy8GameTest {
         game = new Crazy8Game(null);
 
         ArrayList<String> cards = game.getDeck();
-        ArrayList<String> cardAsString = new ArrayList<>();
-        for(int i = 0; i < cards.size(); i++){
-            cardAsString.add(cards.get(i).toString());
-        }
+
         String[] deck = {
-                "AD","AC","AH","AS",
-                "2D","2C","2H","2S",
-                "3D","3C","3H","3S",
-                "4D","4C","4H","4S",
-                "5D","5C","5H","5S",
-                "6D","6C","6H","6S",
-                "7D","7C","7H","7S",
-                "8D","8C","8H","8S",
-                "9D","9C","9H","9S",
-                "10D","10C","10H","10S",
-                "JD","JC","JH","JS",
-                "QD","QC","QH","QS",
-                "KD","KC","KH","KS",
+                "AD", "AC", "AH", "AS",
+                "2D", "2C", "2H", "2S",
+                "3D", "3C", "3H", "3S",
+                "4D", "4C", "4H", "4S",
+                "5D", "5C", "5H", "5S",
+                "6D", "6C", "6H", "6S",
+                "7D", "7C", "7H", "7S",
+                "8D", "8C", "8H", "8S",
+                "9D", "9C", "9H", "9S",
+                "10D", "10C", "10H", "10S",
+                "JD", "JC", "JH", "JS",
+                "QD", "QC", "QH", "QS",
+                "KD", "KC", "KH", "KS",
         };
 
 
-        assertEquals(52,cards.size());
-        for(int i = 0; i < cardAsString.size(); i++){
-            assertTrue(cardAsString.contains(deck[i]));
+        assertEquals(52, cards.size());
+        for (int i = 0; i < cards.size(); i++) {
+            assertTrue(cards.contains(deck[i]));
         }
 
 
