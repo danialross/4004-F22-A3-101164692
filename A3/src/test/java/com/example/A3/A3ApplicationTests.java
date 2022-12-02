61efdc76d0cc88e10c39fb272d53eb8b978e67d9
danialross <92746365+danialross@users.noreply.github.com>
2022-12-02 11:50:24 -0500

    <A-Test>
    row 41

--- A3/src/test/java/com/example/A3/A3ApplicationTests.java
+++ A3/src/test/java/com/example/A3/A3ApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.A3;
 
+import com.example.A3.model.Crazy8Game;
+import com.example.A3.model.Player;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 @SpringBootTest
 class A3ApplicationTests {
 
+
+	ArrayList<Player> players = new ArrayList<>(List.of(
+			new Player(null,"p1"),
+			new Player(null,"p2"),
+			new Player(null,"p3"),
+			new Player(null,"p4")));
+	Crazy8Game game = new Crazy8Game(players);
 	@Test
-	void contextLoads() {
+	void row41() {
+		//draw card as rigging instead of setting hand to have 3C
+		game.drawCard("3C");
+		game.playCard("3C");
+		assertThat(this.game.getCurrPlayerIndex(), is(1));
+
 	}
 
 }
