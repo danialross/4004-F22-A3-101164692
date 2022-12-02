3bcb8c9edc9ddf69598d31f23f670e4980a7f406
danialross <92746365+danialross@users.noreply.github.com>
2022-12-02 12:31:26 -0500

    <A-Test>
    row 44 - 48

--- A3/src/test/java/com/example/A3/A3ApplicationTests.java
+++ A3/src/test/java/com/example/A3/A3ApplicationTests.java
@@ -45,5 +45,54 @@ class A3ApplicationTests {
 		assertThat(game.getCurrPlayerIndex(),is(2));
 
 	}
-	
+
+	@Test
+	void row44(){
+		game.getPlayers().get(0).setHand(new ArrayList<>(List.of("QC")));
+
+		game.playCard("QC");
+		assertThat(game.notifyAction(3),is("a Queen was played, the next player's turn has been skipped"));
+		assertThat(this.game.getCurrPlayerIndex(), is(2));
+
+	}
+
+	@Test
+	void row45(){
+		//setting p4 as current player
+		game.setCurrPlayerIndex(3);
+		game.getPlayers().get(3).setHand(new ArrayList<>(List.of("3C")));
+
+		game.playCard("3C");
+		assertThat(this.game.getCurrPlayerIndex(), is(0));
+
+	}
+
+	@Test
+	void row46(){
+		//setting p4 as current player
+		game.setCurrPlayerIndex(3);
+		game.getPlayers().get(3).setHand(new ArrayList<>(List.of("AH")));
+		game.getPlayers().get(2).setHand(new ArrayList<>(List.of("7H")));
+
+		game.playCard("AC");
+		assertThat(this.game.getCurrPlayerIndex(), is(2));
+		assertThat(game.notifyAction(2),is("an Ace was played, the direction has been reversed"));
+		assertThat(game.showDirection(),is("Current Direction is Right"));
+
+		game.playCard("7H");
+		assertThat(this.game.getCurrPlayerIndex(), is(1));
+
+	}
+
+	@Test
+	void row48(){
+		//setting p4 as current player
+		game.setCurrPlayerIndex(3);
+		game.getPlayers().get(3).setHand(new ArrayList<>(List.of("QC")));
+
+		game.playCard("QC");
+		assertThat(game.notifyAction(3),is("a Queen was played, the next player's turn has been skipped"));
+		assertThat(this.game.getCurrPlayerIndex(), is(1));
+
+	}
 }
