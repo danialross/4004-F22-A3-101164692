dc2e9a40e4f5ce2fcfa71a02ea9d76ce9b3a637f
danialross <92746365+danialross@users.noreply.github.com>
2022-12-04 13:42:19 -0500

    UI
    input validation for user

--- A3/src/main/java/com/example/A3/controller/ChatController.java
+++ A3/src/main/java/com/example/A3/controller/ChatController.java
@@ -10,10 +10,10 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 @Controller
@@ -28,7 +28,60 @@ public class ChatController {
     @MessageMapping("/chat.send")
     @SendTo("/topic/public")
     public void sendMessage(@Payload final ChatMessage chatMessage){
-        sendingOperations.convertAndSend("/topic/public",chatMessage);
+        if(chatMessage.getSender().equals(game.getPlayers().get(game.getCurrPlayerIndex()).getName())){
+
+            //less than 0 DNE
+            //more than 5 == invalid
+            //accept only 2 or 5
+            //reject everything else
+            String[] response = new String[2];
+            if(chatMessage.getContent().length()!=5 && chatMessage.getContent().length()!=2 ){
+                sendInvalidInputError();
+                System.out.println("!5 ||  !2");
+                return;
+
+            } else if(chatMessage.getContent().length()==5){
+
+                if(!chatMessage.getContent().contains(",")){
+                    sendInvalidInputError();
+                    System.out.println("!,");
+                    return;
+                }
+
+                response = chatMessage.getContent().split(",");
+
+                // if left isnt 2 right also isnt
+                if(response[0].length()!=2){
+                    System.out.println("left not 2");
+                    sendInvalidInputError();
+                    return;
+                }
+
+
+            }else if(chatMessage.getContent().length()==2){
+                response[0] = chatMessage.getContent();
+            }
+
+            final ChatMessage msg = ChatMessage.builder()
+                    .type(MessageType.CHAT)
+                    .sender("Dealer")
+                    .content(Arrays.toString(response))
+                    .build();
+            sendingOperations.convertAndSend("/topic/public",msg);
+
+        }else{
+            final ChatMessage msg = ChatMessage.builder()
+                    .type(MessageType.CHAT)
+                    .sender("Dealer")
+                    .content("It is not your turn")
+                    .build();
+            for(Player p: players){
+                if(chatMessage.getSender().equals(p.getName())){
+                    sendingOperations.convertAndSendToUser(p.getUser().getName(), "/topic/private.messages",msg);
+                }
+            }
+
+        }
     }
 
     @MessageMapping("/chat.newUser")
@@ -39,14 +92,52 @@ public class ChatController {
         players.add(new Player(headerAccessor.getUser(), chatMessage.getSender()));
         sendingOperations.convertAndSend("/topic/public",chatMessage);
         if(players.size()>=4){
-            final ChatMessage newMessage = ChatMessage.builder()
+            announceGameStart();
+
+            game = new Crazy8Game(players);
+            game.dealerDrawCard(null);
+            game.dealPlayerCards(new String[][]{{null,null,null,null,null},{null,null,null,null,null},{null,null,null,null,null},{null,null,null,null,null}});
+
+            sendCurrentCardAndTurn();
+            sendPlayerHand();
+        }
+    }
+    public void announceGameStart(){
+        final ChatMessage msg = ChatMessage.builder()
+                .type(MessageType.CHAT)
+                .sender("Dealer")
+                .content("4 Players Have Joined, Game Is Starting\n\n hi")
+                .build();
+        sendingOperations.convertAndSend("/topic/public",msg);
+    }
+    public void sendCurrentCardAndTurn(){
+        final ChatMessage msg = ChatMessage.builder()
+                .type(MessageType.CHAT)
+                .sender("Dealer")
+                .content(game.showCardAndTurn())
+                .build();
+        sendingOperations.convertAndSend("/topic/public",msg);
+
+
+    }
+
+    public void sendPlayerHand(){
+        for(Player p: game.getPlayers()){
+            final ChatMessage playerHand = ChatMessage.builder()
                     .type(MessageType.CHAT)
                     .sender("Dealer")
-                    .content("4 Players Have Joined, Game Is Starting")
+                    .content("Your hand : " + p.getHand().toString())
                     .build();
-            sendingOperations.convertAndSend("/topic/public",newMessage);
-            game = new Crazy8Game(players);
+            sendingOperations.convertAndSendToUser(p.getUser().getName(), "/topic/private.messages",playerHand);
         }
     }
 
+    public void sendInvalidInputError(){
+        final ChatMessage msg = ChatMessage.builder()
+                .type(MessageType.CHAT)
+                .sender("Dealer")
+                .content("Invalid Response, write the ranks and the suits, Eg.'8H' or 'KH,3C'.")
+                .build();
+        sendingOperations.convertAndSendToUser(game.getPlayers().get(game.getCurrPlayerIndex()).getUser().getName(), "/topic/private.messages",msg);
+    }
 }
